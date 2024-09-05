efc97b02605cbd20e10d6a181656b06c3b81efc5
Vharsha <abc@gmail.com>
2024-09-05 10:59:11 +0530

    Started

---
+++ src/main/java/com/ftest/ftest/App.java
@@ -0,0 +1,30 @@
+package com.ftest.ftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.boot.greetingapp.GreetingappApplication;
+
+@RestController
+@SpringBootApplication
+
+public class App {
+	
+    public static void main(String[] args) {
+    	SpringApplication.run(GreetingappApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello() {
+    	return "Hi! Welcome to VTeam";
+    }
+    @GetMapping("/welcome")
+    public String welcome() {
+    	return "Welcome to Verizon";
+    }
+    @GetMapping("/farewell")
+    public String farewell() {
+    	return "Goodbye, see you soon";
+    }
+}
